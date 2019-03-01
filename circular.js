539814d7592dd808d76fe32b7690480b842fb3e6
Klaus Sinani <klaussinani@gmail.com>
2019-03-01 22:02:21 +0200

    Minor repository clean-up

--- circular.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const List = require('./list');
-
-class Circular extends List {}
-
-module.exports = Circular;
--- linear.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const List = require('./list');
-
-class Linear extends List {}
-
-module.exports = Linear;
