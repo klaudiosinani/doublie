d9d1810d38c1e77dee8186ca3adae2aa8b473bb5
Klaus Sinani <klaussinani@gmail.com>
2019-03-23 21:22:52 +0200

    Expose `Node` class constructor to API (#2)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const Circular = require('./circular');
 const Linear = require('./linear');
+const Node = require('./src/node');
 
-module.exports = Object.assign({}, {Circular}, {Linear});
+module.exports = Object.assign({}, {Circular}, {Linear}, {Node});
