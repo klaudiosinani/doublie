ad0bde9797f246d68acb650621998bfdb83621c1
Klaus Sinani <klaussinani@gmail.com>
2019-03-25 21:28:30 +0200

    Add `Linear#append(...values)` class method

--- src/linear.js
+++ src/linear.js
@@ -1,7 +1,40 @@
 'use strict';
 const List = require('./list');
+const Node = require('./node');
 
 class Linear extends List {
+  _initializeList(value) {
+    const node = new Node(value);
+    this._head = node;
+    this._last = node;
+    this._length++;
+  }
+
+  _appendHead(value) {
+    const {_head} = this;
+    const node = new Node(value);
+    node.next = _head;
+    this._head.previous = node;
+    this._head = node;
+    this._length++;
+  }
+
+  _appendLast(value) {
+    const {_last} = this;
+    const node = new Node(value);
+    node.prev = _last;
+    this._last.next = node;
+    this._last = node;
+    this._length++;
+  }
+
+  append(...values) {
+    values.forEach(value => {
+      return this.isEmpty() ? this._initializeList(value) : this._appendLast(value);
+    });
+    return this;
+  }
+
   forEach(fn) {
     let {_head: node} = this;
 
