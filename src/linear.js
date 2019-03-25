411c4882f8c0ceb8d2dd601b239c7e45c75cf60d
Klaus Sinani <klaussinani@gmail.com>
2019-03-25 21:25:49 +0200

    Add `List#forEach(fn)` class method

--- src/linear.js
+++ src/linear.js
@@ -1,6 +1,17 @@
 'use strict';
 const List = require('./list');
 
-class Linear extends List {}
+class Linear extends List {
+  forEach(fn) {
+    let {_head: node} = this;
+
+    while (node) {
+      fn(node.value);
+      node = node.next;
+    }
+
+    return this;
+  }
+}
 
 module.exports = Linear;
