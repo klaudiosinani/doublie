955b7040d5f06d7d74bfebb68a18587efe9ff43d
Klaus Sinani <klaussinani@gmail.com>
2019-03-28 17:04:10 +0200

    Add new `Circular#append(...values)` n-ary class method (#7)

--- src/circular.js
+++ src/circular.js
@@ -1,6 +1,38 @@
 'use strict';
 const List = require('./list');
+const Node = require('./node');
 
-class Circular extends List {}
+class Circular extends List {
+  _addLast(value) {
+    const {_head, _last} = this;
+    const node = new Node(value);
+    node.prev = _last;
+    node.next = _head;
+    this._head.prev = node;
+    this._last.next = node;
+    this._last = node;
+    this._length++;
+  }
+
+  _initializeList(value) {
+    const node = new Node(value);
+    node.next = node;
+    node.prev = node;
+    this._head = node;
+    this._last = node;
+    this._length++;
+  }
+
+  append(...values) {
+    values.forEach(value => {
+      if (this.isEmpty()) {
+        return this._initializeList(value);
+      }
+
+      return this._addLast(value);
+    });
+    return this;
+  }
+}
 
 module.exports = Circular;
